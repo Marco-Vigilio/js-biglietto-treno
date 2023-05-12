123bd1573cc947ba29b91d54c913d8dd9a4971fb
Marco-Vigilio <tonyvigilio@gmail.com>
2023-05-12 15:49:04 +0200

    analizzo se il numero scritto dall'utente è un numero

--- script/myscript.js
+++ script/myscript.js
@@ -1,21 +1,25 @@
-/** 
+/**
 //todo: 
-Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
-Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
-il prezzo del biglietto è definito in base ai km (0.21 € al km)
+Il programma dovrà chiedere all'utente 
+il numero di chilometri che vuole percorrere e l'età 
+del passeggero.
+Sulla base di queste informazioni dovrà calcolare 
+il prezzo totale del viaggio, secondo queste regole:
+il prezzo del biglietto 
+è definito in base ai km (0.21 € al km)
 va applicato uno sconto del 20% per i minorenni
 va applicato uno sconto del 40% per gli over 65.
-
-
 */
 
-/* 
- * importante
- ! qualcosa
- ? qualcosa
-TODO:
+let numerokm;
+let età;
 
-*/
+numerokm = parseInt(prompt("Inserisci il numero di kilomentri che devi fare: "));
+if (numerokm === 3){
+    console.log("questo è un numero");
+    console.log("Il passeggero deve fare: "+ numerokm + "km");
 
-//* commento
-
+}
+else{
+    console.log("Deve inserire un numero, ricarichi la pagina");
+}
